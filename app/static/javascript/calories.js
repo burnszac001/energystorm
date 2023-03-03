d1817172cb53071eff14fcc60a9a0f6a4d0fb3b4
Zachary Burns <burnszac001@gmail.com>
2023-03-03 11:30:48 -0700

    created feature to add food items

--- app/static/javascript/calories.js
+++ app/static/javascript/calories.js
@@ -9,11 +9,36 @@ function hideItem (id) {
 }
 
 class FoodItem {
-    #name;
-    #calories;
+    name;
+    calories;
+    elemId;
+    #id;
 
-    constructor (foodName) {
-        this.#name = foodName;
+    constructor (foodName, calorieAmount, id) {
+        this.name = foodName;
+        this.calories = calorieAmount;
+        this.#id = id;
+        this.elemId = `food-item-${this.id}`;
+    }
+
+    displayFoodItem () {
+        const newElem = document.createElement(`div`);
+        let textElem = document.createTextNode(`${this.name}: ${this.calories} calories`);
+        newElem.appendChild(textElem);
+        const btn = document.createRange().createContextualFragment(
+            `<button onclick="tracker.deleteFoodItem(${this.#id})">Delete</button>`
+        );
+        newElem.appendChild(btn);
+        newElem.setAttribute("id", this.elemId);
+
+        const foodItemsElem = document.getElementById("food-items");
+        foodItemsElem.appendChild(newElem);
+    }
+
+
+    deleteSelf () {
+        const self = document.getElementById(this.elemId);
+        self.parentNode.removeChild(self);
     }
 
     addCalories () {}
@@ -22,27 +47,56 @@ class FoodItem {
 
 class CalorieTracker {
     dailyCalorieGoal;
+    remainingCalories;
+    #foodItemsSize = 0;
     #foodItems = [];
 
     constructor (goal) {
         this.dailyCalorieGoal = goal;
+        this.remainingCalories = goal;
     }
+
+    displayRemainingCalories () {
+        const remainingCaloriesElem = document.getElementById("calories-remaining");
+        remainingCaloriesElem.innerHTML = `${tracker.remainingCalories}`;
+    }
+
 
     updateCalorieGoal () {
         const goalInputEl = document.querySelector("#goal-input");
         const calorieGoalEl = document.querySelector("#calorie-goal");
+
+        this.remainingCalories -= (this.dailyCalorieGoal - goalInputEl.value);
         this.dailyCalorieGoal = goalInputEl.value;
         calorieGoalEl.innerHTML = `${this.dailyCalorieGoal}`;
+
         hideItem('setCalorieGoal');
         showItem('change-btn');
+        this.displayRemainingCalories();
     }
 
+
     addFoodItem () {
-        newFoodItem = new FoodItem(foodName);
-        this.#foodItems.push(newFoodItem);
+        const itemElems = document.getElementsByClassName("add-food-item");
+
+        const foodItem = new FoodItem(itemElems[0].value, itemElems[1].value, this.#foodItemsSize)
+        foodItem.displayFoodItem();
+        this.#foodItems.push(foodItem);
+        this.#foodItemsSize++;
+
+        this.remainingCalories -= foodItem.calories;
+        this.displayRemainingCalories();        
     }
+
+    deleteFoodItem (index) {
+        const foodItem = this.#foodItems[index];
+        foodItem.deleteSelf();
+        this.#foodItems.splice(index, 1);
+    }
+
 }
 
 const tracker = new CalorieTracker(2000);
 const calorieGoalEl = document.querySelector("#calorie-goal");
 calorieGoalEl.innerHTML = `${tracker.dailyCalorieGoal}`;
+tracker.displayRemainingCalories();
